b59072b37046ebfdf63c10e3093bd6265a9c03e1
Jaka <jaka0926@naver.com>
2024-05-20 00:23:25 +0900

    Initial Commit

---
+++ SeSAC-Assignment-4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSAC-Assignment-4
+//
+//  Created by Jaka on 2024-05-20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
