947f993928f801c4d50e1f6a3aea0ee91754e521
Jaka <jaka0926@naver.com>
2024-05-22 20:13:04 +0900

    EmotionView - 저장 및 리셋 기능 추가

--- SeSAC-Assignment-4/Codes/emotionViewController.swift
+++ SeSAC-Assignment-4/Codes/emotionViewController.swift
@@ -38,6 +38,26 @@ class emotionViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        let saved1 = UserDefaults.standard.integer(forKey: "label1")
+        let saved2 = UserDefaults.standard.integer(forKey: "label2")
+        let saved3 = UserDefaults.standard.integer(forKey: "label3")
+        let saved4 = UserDefaults.standard.integer(forKey: "label4")
+        let saved5 = UserDefaults.standard.integer(forKey: "label5")
+        let saved6 = UserDefaults.standard.integer(forKey: "label6")
+        let saved7 = UserDefaults.standard.integer(forKey: "label7")
+        let saved8 = UserDefaults.standard.integer(forKey: "label8")
+        let saved9 = UserDefaults.standard.integer(forKey: "label9")
+        
+        labelNum[0] = saved1
+        labelNum[1] = saved2
+        labelNum[2] = saved3
+        labelNum[3] = saved4
+        labelNum[4] = saved5
+        labelNum[5] = saved6
+        labelNum[6] = saved7
+        labelNum[7] = saved8
+        labelNum[8] = saved9
+        
         deleteButtonTitle(image1)
         deleteButtonTitle(image2)
         deleteButtonTitle(image3)
@@ -57,7 +77,11 @@ class emotionViewController: UIViewController {
         setLabelprops(label7)
         setLabelprops(label8)
         setLabelprops(label9)
+        
+        
     }
+    
+    
     
     
     func deleteButtonTitle(_ image: UIButton) {
@@ -67,44 +91,67 @@ class emotionViewController: UIViewController {
     func setLabelprops(_ label: UILabel) {
         label.textAlignment = .center
         label.textColor = .white
-        label.text = labelArr[label.tag]
+        label.text = labelArr[label.tag] + " \(labelNum[label.tag])"
     }
     
     @IBAction func Image1clicked(_ sender: UIButton) {
         labelNum[sender.tag] += 1
         label1.text = labelArr[sender.tag] + " \(labelNum[sender.tag])"
+        UserDefaults.standard.set(labelNum[sender.tag], forKey: "label1")
     }
     
     @IBAction func Image2clicked(_ sender: UIButton) {
         labelNum[sender.tag] += 1
         label2.text = labelArr[sender.tag] + " \(labelNum[sender.tag])"
+        UserDefaults.standard.set(labelNum[sender.tag], forKey: "label2")
     }
     @IBAction func Image3clicked(_ sender: UIButton) {
         labelNum[sender.tag] += 1
         label3.text = labelArr[sender.tag] + " \(labelNum[sender.tag])"
+        UserDefaults.standard.set(labelNum[sender.tag], forKey: "label3")
     }
     @IBAction func Image4clicked(_ sender: UIButton) {
         labelNum[sender.tag] += 1
         label4.text = labelArr[sender.tag] + " \(labelNum[sender.tag])"
+        UserDefaults.standard.set(labelNum[sender.tag], forKey: "label4")
     }
     @IBAction func Image5clicked(_ sender: UIButton) {
         labelNum[sender.tag] += 1
         label5.text = labelArr[sender.tag] + " \(labelNum[sender.tag])"
+        UserDefaults.standard.set(labelNum[sender.tag], forKey: "label5")
     }
     @IBAction func Image6clicked(_ sender: UIButton) {
         labelNum[sender.tag] += 1
         label6.text = labelArr[sender.tag] + " \(labelNum[sender.tag])"
+        UserDefaults.standard.set(labelNum[sender.tag], forKey: "label6")
     }
     @IBAction func Image7clicked(_ sender: UIButton) {
         labelNum[sender.tag] += 1
         label7.text = labelArr[sender.tag] + " \(labelNum[sender.tag])"
+        UserDefaults.standard.set(labelNum[sender.tag], forKey: "label7")
     }
     @IBAction func Image8clicked(_ sender: UIButton) {
         labelNum[sender.tag] += 1
         label8.text = labelArr[sender.tag] + " \(labelNum[sender.tag])"
+        UserDefaults.standard.set(labelNum[sender.tag], forKey: "label8")
     }
     @IBAction func Image9clicked(_ sender: UIButton) {
         labelNum[sender.tag] += 1
         label9.text = labelArr[sender.tag] + " \(labelNum[sender.tag])"
+        UserDefaults.standard.set(labelNum[sender.tag], forKey: "label9")
+    }
+    
+    
+    @IBAction func resetButton(_ sender: UIButton) {
+        labelNum = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+        setLabelprops(label1)
+        setLabelprops(label2)
+        setLabelprops(label3)
+        setLabelprops(label4)
+        setLabelprops(label5)
+        setLabelprops(label6)
+        setLabelprops(label7)
+        setLabelprops(label8)
+        setLabelprops(label9)
     }
 }
